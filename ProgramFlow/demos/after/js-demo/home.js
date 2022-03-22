10c0a7cf2d9100d2a1dfc7bf68dc5d0f349d7b71
Richard Mery <richard.mery@appius.com>
2022-03-22 16:52:59 +0000

    course file amends

--- ProgramFlow/demos/after/js-demo/home.js
+++ ProgramFlow/demos/after/js-demo/home.js
@@ -4,3 +4,15 @@ do {
     console.log(i);
     i--;
 } while (i > 0);
+
+
+// + symbol leaves the number as is and - symbol makes the number a negative number
+// However, the + symbol converts string to number in the below scenario
+
+let price = +(1.1 + 1.3).toFixed(2) === 2.4;
+
+if( price === 2.4){
+    showMessage('true');
+} else if (price !== 2.4){
+    showMessage('false');
+}
