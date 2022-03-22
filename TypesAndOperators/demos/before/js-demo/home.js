07e60f602b79fa342ba8f9c319c0054e3f848ace
Richard Mery <richard.mery@appius.com>
2022-03-22 11:12:50 +0000

    progress

--- TypesAndOperators/demos/before/js-demo/home.js
+++ TypesAndOperators/demos/before/js-demo/home.js
@@ -1,7 +1,16 @@
 
+//Assignement operator =
+// ++ increment operator - add to the left of the variable that you want it assigned to. Not doing so will create a bug
+// -- decrement operator - add to the left of the variable that you want it assigned to. Not doing so will create a bug
+
+// Operator precedence
+// e.g https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
 
 
-let price = 25;
+
+let price = 25.99;
+price += 5;
+
 
 showMessage(price);
-console.log(price);
+console.log(typeof price);
--- VariablesAndConstants/demos/before/js-demo/home.js
+++ VariablesAndConstants/demos/before/js-demo/home.js
@@ -1,8 +1,15 @@
 // show the title
 showMessage("Title...");
-let price = 149.99;
+//let price = 149.99;
 /*
     Detail complex logic...
     Some algorithm...
 
 */
+
+
+let price = 49.99,
+title = "Hiking Boots",
+discounted = false;
+
+showMessage(price);
