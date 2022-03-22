cf55c33ca9e2f9d22405414c73c12f86cbb7d989
Richard Mery <richard.mery@appius.com>
2022-03-22 14:54:13 +0000

    course notes

--- TypesAndOperators/demos/before/js-demo/home.js
+++ TypesAndOperators/demos/before/js-demo/home.js
@@ -1,16 +1,26 @@
-
 //Assignement operator =
 // ++ increment operator - add to the left of the variable that you want it assigned to. Not doing so will create a bug
 // -- decrement operator - add to the left of the variable that you want it assigned to. Not doing so will create a bug
 
 // Operator precedence
-// e.g https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+// e.g. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
 
-
+// Escape notation
+// e.g. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
 
 let price = 25.99;
 price += 5;
 
+let person = {
+    firstName: 'Rich',
+    lastName: 'Mery'
+};
 
-showMessage(price);
-console.log(typeof price);
+let userName = 'Rich';
+let message = `Discount for`;
+//let message = 123;
+
+
+showMessage(message + "\n" + person.firstName + " " + person.lastName);
+// console.log(person);
+// console.log(typeof person);
