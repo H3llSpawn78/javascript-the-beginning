97b4d32cdc342dd42b7f27dddee0e1b85815a61c
Richard Mery <richard.mery@appius.com>
2022-04-04 13:06:23 +0100

    course file amends

--- Arrays/demos/after/js-demo/home.js
+++ Arrays/demos/after/js-demo/home.js
@@ -3,3 +3,23 @@ const containers =
     document.getElementsByClassName('container');
 containers[0].classList.add('d-none');
 console.log(containers);
+
+
+
+// Show and hide section on click
+const button = document.getElementById('see-review');
+
+button.addEventListener('click', function(event) {
+
+    const review = document.getElementById('review');
+
+    if (review.classList.contains('transition-height')) {
+        review.classList.remove('transition-height');
+        button.textContent = 'SEE REVIEW';
+    }
+    else {
+        review.classList.add('transition-height');
+        button.textContent = 'CLOSE REVIEW';
+    }
+
+});
